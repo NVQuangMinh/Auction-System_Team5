91e8dc595ddc18d369a525011e07c5fc27fb32a5
Hutao1507 <25021882@vnu.edu.vn>
2026-03-30 09:37:39 +0700

    make sigup scene

---
+++ AUCTION_SYSTEM_TEAM5/src/main/java/project/auction_system_team5/SignUpSceneController.java
@@ -0,0 +1,20 @@
+package project.auction_system_team5;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.event.ActionEvent;
+
+
+public class SignUpSceneController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField cfpassword;
+
+    @FXML
+    protected void create_account(ActionEvent)
