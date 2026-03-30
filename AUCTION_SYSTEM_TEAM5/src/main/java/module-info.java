86f7b47ef06c0b6c91a442beeaf59e5627393813
VUMINH <minhquangvu201@gmail.com>
2026-03-30 09:35:57 +0700

    Sign in scene

--- AUCTION_SYSTEM_TEAM5/src/main/java/module-info.java
+++ AUCTION_SYSTEM_TEAM5/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module project.auction_system_team5 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens project.auction_system_team5 to javafx.fxml;
