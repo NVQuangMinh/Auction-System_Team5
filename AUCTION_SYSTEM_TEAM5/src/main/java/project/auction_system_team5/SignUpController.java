c5115f89edb6a1b39cd90b97015d484f5885905d
Hutao1507 <25021882@vnu.edu.vn>
2026-03-30 10:34:27 +0700

    make sigup scene

--- AUCTION_SYSTEM_TEAM5/src/main/java/project/auction_system_team5/SignUpController.java
+++ AUCTION_SYSTEM_TEAM5/src/main/java/project/auction_system_team5/SignUpController.java
@@ -1,5 +1,30 @@
 package project.auction_system_team5;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SignUpController {
+    public PasswordField password;
+    public PasswordField confirmpassword;
+    public TextField username;
+    Stage stage;
+    Scene scene;
+    Parent root;
+
+    public void switchToSignInScene(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("SignInScene.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
