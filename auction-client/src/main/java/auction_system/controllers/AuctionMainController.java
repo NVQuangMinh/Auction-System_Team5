e6dd4b22d6d0b732b31349777fbff0091de36910
VUMINH <minhquangvu201@gmail.com>
2026-04-11 10:01:03 +0700

    aution-shared

---
+++ auction-client/src/main/java/auction_system/controllers/AuctionMainController.java
@@ -0,0 +1,12 @@
+package auction_system.controllers;
+
+import javafx.fxml.FXML;
+
+public class AuctionMainController {
+    @FXML
+    public WebMenuBarController webMenuBarController;
+
+    public WebMenuBarController getWebMenuBarController(){
+        return webMenuBarController;
+    }
+}
---
+++ auction-client/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module project.auction_system_team5 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    // Cho phép JavaFX truy cập vào class khởi chạy (Launcher, Auction)
+    opens auction_system.launcher to javafx.graphics, javafx.fxml;
+
+    // Cho phép JavaFX FXML load các Controller
+    opens auction_system.controllers to javafx.fxml;
+
+    // Xuất các package để các module khác có thể dùng (nếu cần)
+    exports auction_system.launcher;
+    exports auction_system.controllers;
+}
