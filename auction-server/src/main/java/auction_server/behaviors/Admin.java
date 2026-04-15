e1226718409bdf9284a66146f964482bc38b7671
VUMINH <minhquangvu201@gmail.com>
2026-04-15 16:27:39 +0700

    add

--- auction-server/src/main/java/auction_server/behaviors/Admin.java
+++ auction-server/src/main/java/auction_server/behaviors/Admin.java
@@ -1,4 +1,13 @@
 package auction_server.behaviors;
 
+import auction_server.entities.Auction;
+import auction_server.entities.User;
+
 public class Admin {
+    public void cancelAuction(Auction auction){
+
+    }
+    public void banUser(User user){
+
+    }
 }
