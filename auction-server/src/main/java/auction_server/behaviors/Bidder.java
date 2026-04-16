9e0c72b4e18b504368c456169c467f9581f4a533
VUMINH <minhquangvu201@gmail.com>
2026-04-16 17:14:17 +0700

    add

--- auction-server/src/main/java/auction_server/behaviors/Bidder.java
+++
@@ -1,7 +0,0 @@
-package auction_server.behaviors;
-
-import auction_server.interfaces.CanBid;
-
-public class Bidder implements CanBid {
-    public void placeBid(String itemId, double amount) {}
-}
--- auction-server/src/main/java/auction_server/interfaces/CanSell.java
+++
@@ -1,7 +0,0 @@
-package auction_server.interfaces;
-
-import auction_server.entities.Item;
-
-public interface CanSell {
-    void postItem(Item item);
-}
