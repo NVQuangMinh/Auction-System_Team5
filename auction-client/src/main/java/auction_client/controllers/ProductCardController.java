85915f210cfd7a2faeee4b09b438f685c4d60d20
VUMINH <minhquangvu201@gmail.com>
2026-04-24 10:12:03 +0700

    crucial change and addition

--- auction-client/src/main/java/auction_client/controllers/ProductCardController.java
+++ auction-client/src/main/java/auction_client/controllers/ProductCardController.java
@@ -1,8 +1,13 @@
 package auction_client.controllers;
 
+import auction_client.Network.ClientService;
+import auction_shared.Network.NetworkMessage;
 import auction_shared.entities.Auction;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+
 
 public class ProductCardController {
     @FXML
@@ -14,10 +19,19 @@ public class ProductCardController {
     @FXML
     protected Label description;
 
+    Auction auction = null;
+
     public void setData(Auction auction) {
+        this.auction = auction;
         itemName.setText(auction.getItem().getName());
         itemState.setText(auction.getState());
         currentPrice.setText(String.valueOf(auction.getCurrentHighestBid()));
         description.setText(auction.getItem().getDescription());
+    }
+
+    @FXML
+    public void buyOut(ActionEvent event){
+        ClientService.getInstance().sendMessage(new NetworkMessage("BUY_OUT", auction));
+
     }
 }
