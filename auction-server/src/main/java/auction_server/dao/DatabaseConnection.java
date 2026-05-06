5d5185d4d7cba0b14e851cb437d3d98b6a2b4152
heisenberg <blizzerphuong@gmail.com>
2026-05-07 00:01:33 +0700

    note lại cho DatabaseConnection.java

--- auction-server/src/main/java/auction_server/dao/DatabaseConnection.java
+++ auction-server/src/main/java/auction_server/dao/DatabaseConnection.java
@@ -11,17 +11,26 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final Logger log = LoggerFactory.getLogger(DatabaseConnection.class);
-    private static HikariDataSource dataSource = null;
 
+    // Dùng thư viện HikariCP để quản lý connection pool, tốc độ nhanh hơn so với 1 connection
+    private static HikariDataSource dataSource = null;
+    // Singleton
     private DatabaseConnection() {}
 
     private static synchronized void init() {
         if (dataSource != null) return;
+
+        // đối tượng config ở đây là bản thiết kế lưu trữ thông tin kết nối đi đâu, dùng tài khoản nào,
+        // hồ chứa được phép chứa tối đa bao nhiêu kết nối,...
         HikariConfig config = new HikariConfig();
+
+        // dùng các biến môi trường để lấy thông tin DB
         config.setJdbcUrl(System.getenv("DB_URL"));
         config.setUsername(System.getenv("DB_USER"));
         config.setPassword(System.getenv("DB_PASSWORD"));
         config.setMaximumPoolSize(10);
+
+        // nạp vào HikariDataSource để khởi chạy.
         dataSource = new HikariDataSource(config);
         log.info("Database connection pool initialized.");
     }
