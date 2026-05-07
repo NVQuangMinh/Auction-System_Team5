c6821f920f966a14e30768cf4ad9c8d95d04812b
Hutao1507 <25021882@vnu.edu.vn>
2026-05-07 17:17:17 +0700

    FIX info window

--- auction-client/src/main/java/auction_client/controllers/BidProductInfoController.java
+++ auction-client/src/main/java/auction_client/controllers/BidProductInfoController.java
@@ -1,21 +1,40 @@
 package auction_client.controllers;
 
-import auction_client.interfaces.AuctionUpdateListener;
 import auction_client.Network.ClientService;
-import auction_client.UserSession;
+import auction_client.interfaces.AuctionUpdateListener;
 import auction_shared.Network.NetworkMessage;
 import auction_shared.dto.AuctionDTO;
-import auction_shared.dto.BidTransactionDTO;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class SellProductInfoController implements AuctionUpdateListener {
+public class BidProductInfoController implements Initializable, AuctionUpdateListener {
+    @FXML
+    private FlowPane productFlowPane;
+    @FXML
+    private CheckBox autoBidCheck;
+    @FXML
+    private HBox bidContainer;
+    @FXML
+    private HBox autoBidContainer;
     @FXML
     Label itemName;
     @FXML
@@ -34,11 +53,15 @@ public class SellProductInfoController implements AuctionUpdateListener {
 
     AuctionDTO auction = null;
 
-
-    public void initData(AuctionDTO auction){
-        this.auction = auction;
-        updateData();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         ClientService.getInstance().addListener(this);
+        ClientService.getInstance().sendMessage(new NetworkMessage("GET_PRODUCTS", null));
+        bidContainer.managedProperty().bind(bidContainer.visibleProperty());
+        autoBidContainer.managedProperty().bind(autoBidContainer.visibleProperty());
+        autoBidContainer.visibleProperty().bind(autoBidCheck.selectedProperty());
+        bidContainer.visibleProperty().bind(autoBidCheck.selectedProperty().not());
+
     }
 
     public void updateData(){
@@ -59,24 +82,19 @@ public class SellProductInfoController implements AuctionUpdateListener {
                 this.auction = updatedAuction;
                 updateData();
             }
-
         }
     }
 
+    public void setData(AuctionDTO auction) {
+        itemName.setText(auction.getItem().getName());
+        description.setText(auction.getItem().getDescription());
+        currentPrice.setText(String.valueOf(auction.getCurrentHighestBid()));
+        buyOut.setText(String.valueOf(auction.getBuyOutPrice()));
+        tickRate.setText((String.valueOf(auction.getTickSize())));
+    }
     @FXML
     private void switchToUserProductList(MouseEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
-    }
-
-    @FXML
-    public void placeBidRequest(){
-        BidTransactionDTO transaction = new BidTransactionDTO(auction, UserSession.getInstance().getUser(),Double.parseDouble(bidAmount.getText()));
-        ClientService.getInstance().sendMessage(new NetworkMessage("PLACE_BID", transaction));
-    }
-
-
-    public void cleanUp(){
-        ClientService.getInstance().removeListener(this);
     }
 }
