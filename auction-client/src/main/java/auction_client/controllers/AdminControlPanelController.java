611ebf64d892808670f17c2edd9d0b4b2dd82ce9
Hutao1507 <25021882@vnu.edu.vn>
2026-05-12 00:03:41 +0700

    admin control panel controller

--- auction-client/src/main/java/auction_client/controllers/AdminControlPanelController.java
+++ auction-client/src/main/java/auction_client/controllers/AdminControlPanelController.java
@@ -2,5 +2,21 @@ package auction_client.controllers;
 
 import javafx.fxml.Initializable;
 
-public class AdminControlPanelController implements Initializable {\
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminControlPanelController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupUserTable();
+        setupItemTable();
+
+    }
+
+    private void setupUserTable() {
+    }
+
+    private void setupItemTable() {
+    }
 }
