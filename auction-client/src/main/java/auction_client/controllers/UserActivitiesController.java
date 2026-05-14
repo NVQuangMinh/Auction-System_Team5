c5706ad17545b7aa31fda6f27dd5dd78543d2c64
Hutao1507 <25021882@vnu.edu.vn>
2026-05-14 17:55:29 +0700

    user activity

--- auction-client/src/main/java/auction_client/controllers/UserActivitiesController.java
+++ auction-client/src/main/java/auction_client/controllers/UserActivitiesController.java
@@ -3,9 +3,12 @@ package auction_client.controllers;
 import auction_shared.Network.Notification;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -16,6 +19,17 @@ public class UserActivitiesController {
 
     public void loadNotifications(List<Notification> notificationList) {
         notificationContainer.getChildren().clear();
-        for
+        for (Notification notification : notificationList) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/auction_client/ActivitiesItem.fxml"));
+                Parent item = loader.load();
+
+                ActivitiesItemController controller = loader.getController();
+                controller.setData(notification.getNotificationMSG(), notification.getNotificationTime());
+                notificationContainer.getChildren().add(item);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
