ae73211a590d0be75eb6f5d8c0569001a2892831
heisenberg <blizzerphuong@gmail.com>
2026-05-16 18:13:31 +0700

    thêm getAllUsers trong UserDAO

--- auction-server/src/main/java/auction_server/dao/UserDAO.java
+++ auction-server/src/main/java/auction_server/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO {
@@ -52,6 +53,27 @@ public class UserDAO {
     }
 
     public static List<User> getAllUsers() {
-        return null;
+
+        String sql = "SELECT * FROM users WHERE role = 'USER' ";
+
+        List<User> users = new ArrayList<>();
+
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery()) {
+
+            while(rs.next()){
+                User user = new User(
+                        rs.getString("id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("role")
+                );
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 }
