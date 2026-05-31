d4b68434f3d02ae8432d83fb89898d7fc83f738d
Hutao1507 <25021882@vnu.edu.vn>
2026-05-31 20:39:34 +0700

    sua workflow lan 7 (tang tiep thoi gian nghi de FX load)

--- auction-client/src/test/java/auctionclient/controllers/FxControllerTestBase.java
+++ auction-client/src/test/java/auctionclient/controllers/FxControllerTestBase.java
@@ -9,7 +9,7 @@ import org.testfx.util.WaitForAsyncUtils;
  */
 public abstract class FxControllerTestBase extends ApplicationTest {
 
-    private static final int FX_STABILIZE_MS = 600;
+    private static final int FX_STABILIZE_MS = 1000;
 
     @BeforeEach
     void stabilizeFxBeforeTest() {
