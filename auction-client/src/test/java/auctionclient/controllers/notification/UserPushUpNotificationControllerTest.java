e809ff441b50ed2b68e885f24dfb746bd38b7347
Hutao1507 <25021882@vnu.edu.vn>
2026-06-02 13:32:17 +0700

    sửa test

--- auction-client/src/test/java/auctionclient/controllers/notification/UserPushUpNotificationControllerTest.java
+++ auction-client/src/test/java/auctionclient/controllers/notification/UserPushUpNotificationControllerTest.java
@@ -127,11 +127,11 @@ class UserPushUpNotificationControllerTest extends FxControllerTestBase {
         try (MockedStatic<UserPushUpNotificationController> mocked =
                      mockStatic(UserPushUpNotificationController.class)) {
             controller.onUpdateReceived(
-                    new NetworkMessage("BID_FAILED", "Insufficient balance"));
+                    new NetworkMessage("BID_FAILED", "Trả giá thất bại: không tìm thấy phiên đấu giá."));
             org.testfx.util.WaitForAsyncUtils.waitForFxEvents();
 
             mocked.verify(() -> UserPushUpNotificationController.showNotification(
-                    "Insufficient balance", "FAILED"));
+                    "Trả giá thất bại: không tìm thấy phiên đấu giá.", "FAILED"));
         }
     }
 
@@ -152,11 +152,11 @@ class UserPushUpNotificationControllerTest extends FxControllerTestBase {
         try (MockedStatic<UserPushUpNotificationController> mocked =
                      mockStatic(UserPushUpNotificationController.class)) {
             controller.onUpdateReceived(
-                    new NetworkMessage("SELL_FAILED", "Cannot list item"));
+                    new NetworkMessage("SELL_FAILED", "Bạn đã đăng bán sản phẩm thất bại."));
             org.testfx.util.WaitForAsyncUtils.waitForFxEvents();
 
             mocked.verify(() -> UserPushUpNotificationController.showNotification(
-                    "Cannot list item", "FAILED"));
+                    "Bạn đã đăng bán sản phẩm thất bại.", "FAILED"));
         }
     }
 
